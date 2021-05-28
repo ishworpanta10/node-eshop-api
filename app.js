9f48ec9c30dc79ac4c0957cb2df72b365fbb25f5
ishworpanta10 <ishworpanta10@gmail.com>
2021-05-28 22:49:14 +0545

    successsful connection to mongo atlas db

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 // middleware for parsing api
 app.use(express.json());
+// for logginh
+app.use(morgan("tiny"));
 
 require("dotenv/config");
 
 const api = process.env.API_URL;
 
+const connectionString = process.env.CONNECTION_STRING;
+
 // initial route
 app.get(`${api}/products`, (req, res) => {
   //   res.send("Hello API");
+
   const product = {
     id: 1,
     name: "product name test",
@@ -26,6 +32,22 @@ app.post(`${api}/products`, (req, res) => {
 
   res.send(newProduct);
 });
+
+// connecting to db before connecting to server
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "eshopDB",
+  })
+  // success case
+  .then(() => {
+    console.log("Database Connection successful");
+  })
+  // error case
+  .catch((err) => {
+    console.log("Error Connection DB:", err);
+  });
 
 app.listen(3000, () => {
   console.log(`Server Started : http://localhost:3000${api}`);
